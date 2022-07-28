771177dabcb1886930d56124bad0ae879cafc809
Hui Sheng Lim <73118623+xanxustyle@users.noreply.github.com>
2022-07-28 16:25:37 +1000

    Update gpmodel_library.py

--- gpmodel_library.py
+++ gpmodel_library.py
@@ -546,7 +546,7 @@ class SpatialGPModel(GPModel):
         # Calculate for the test point
         assert(xvals.shape[0] >= 1)            
         assert(xvals.shape[1] == self.dimension)
-	n_points, input_dim = xvals.shape
+        n_points, input_dim = xvals.shape
 
         # With no observations, predict 0 mean everywhere and prior variance
         if self.xvals is None:
